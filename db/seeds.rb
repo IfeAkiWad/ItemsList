1562c34de5b9d283a43787a8300a73c4107af61c
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2023-05-23 17:05:42 -0500

    create a seeds for item

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create items with names
+Item.create(name: "Apple")
+Item.create(name: "Banana")
+Item.create(name: "Orange")
+
