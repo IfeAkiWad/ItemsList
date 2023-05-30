fb57af8f6c666d5a7f53e5f5fc160ed28e66f48c
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2023-05-30 11:40:49 -0500

    add read, update, and destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,36 @@
 class ItemsController < ApplicationController
+     before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def index
     @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+
+  def create
+    @items = Item.new(item_params)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_item
+    @item = Item.find_by_id(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name)
   end
 end
