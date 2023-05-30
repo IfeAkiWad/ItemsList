3f9a3957a3706cae6582469acbdfe02f2ad7cb07
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2023-05-29 20:06:21 -0500

    build welcome views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_29_202820) do
+ActiveRecord::Schema.define(version: 2023_05_30_003926) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2023_05_29_202820) do
   end
 
   create_table "welcomes", force: :cascade do |t|
-    t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
