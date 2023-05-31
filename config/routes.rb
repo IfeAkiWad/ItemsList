0e6cc0bbb289401014713bda25ebc4573597dee2
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2023-05-31 14:42:17 -0500

    build views for items and welcome

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'items/index'
   get 'welcome/index'
 
+  resources :items
+
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
